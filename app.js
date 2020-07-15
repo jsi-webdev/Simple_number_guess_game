f310debf78e0e25c608d32c1b16e52ac5e3ee21b
jsi-webdev <j.skullow@gmail.com>
2020-07-16 05:22:05 +0900

    set html and make event for wininig case

---
+++ app.js
@@ -0,0 +1,45 @@
+// jshint esversion:6
+
+// Game Value
+let min = 1,
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector("#game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessInput = document.querySelector("#guess-input"),
+  guessBtn = document.querySelector("#guess-btn"),
+  message = document.querySelector(".message");
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Litsen for guess
+guessBtn.addEventListener("click", function () {
+  let guess = parseInt(guessInput.value);
+
+  // Validate
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage("Please enter a number between " + min + " and " + max, "red");
+    // setMessage("Please enter a number between ${min} and ${max}", "red");
+  // Check if won
+  if (guess === winningNum) {
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = "green";
+    // Set message
+    setMessage(winningNum + " is correct, YOU WIN!", "green");
+  } else {
+  }
+});
+
+// Set message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
